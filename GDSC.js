32be5cc4ae6a2a206d1f70e2a6eb99fbcf6bdcb8
yash chauhan <yash.chauhan.ug20@nsut.ac.in>
2022-01-28 00:44:29 +0530

    Create GDSC.js

---
+++ GDSC.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".header ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight /3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
+
+
+
